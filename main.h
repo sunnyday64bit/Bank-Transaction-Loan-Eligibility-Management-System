d57b64a369135508095a902859646599776c6810
sunnyday64bit <nautiyalansh30@gmail.com>
2025-10-08 22:35:15 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void clear_input_buffer();
+void printMenu();
+
+void handleCreateAccountFromMenu();
+void handleUpdateAccountDetails();
+void handleDisplayHistory();
+void displayAllAccounts();
+
+#endif
