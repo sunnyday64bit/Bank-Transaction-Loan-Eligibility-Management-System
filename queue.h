b2e5c72527506f95d4ee1fad7d2560e042f4ad20
sunnyday64bit <nautiyalansh30@gmail.com>
2025-11-09 13:10:16 +0530

    Update queue.h

--- queue.h
+++ queue.h
@@ -8,6 +8,16 @@ typedef struct ServiceRequest {
     char details[100]; 
 } ServiceRequest;
 
+typedef struct ServiceQueue { 
+    ServiceRequest items[MAX_QUEUE]; 
+    int front; 
+    int rear; 
+    int count; 
+} ServiceQueue;
+
+// Export the global queue structure for status checks (used in report.c)
+extern ServiceQueue serviceQ;
+
 void handleNewServiceRequest();
 void dequeueServiceRequest();
 
